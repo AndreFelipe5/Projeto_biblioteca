28d9118f81cc8e3aa6c862b60fc4b4e54f85668a
AndreFelipe5 <afs69@discente.ifpe.edu.br>
2026-06-11 17:03:32 -0300

    retirado cadastro de usuarios

--- menu.py
+++ menu.py
@@ -1,43 +1,41 @@
-import cadastro_users
 import cadastro_livros
 import emprestimo_livros
 import devolucao_livros
 import relatorio
 import editar_livro
 
+
+
 def menu():
     while True:
-        print('Biblioteca LP')
+        
+        print('=====> Biblioteca LP <=====>\n')
+       
+        print(' 1 - Cadastrar Livro')
+        print(' 2 - Editar Livros')
+        print(' 3 - Emprestimo de Livros')
+        print(' 4 - Devolução de Livros')
+        print(' 5 - Relatorio da Biblioteca')
+        print(' 6 - Sair')
 
-        print(' 1 - Cadastrar Usuário')
-        print(' 2 - Cadastrar Livro')
-        print(' 3 - Editar Livros')
-        print(' 4 - Emprestimo de Livros')
-        print(' 5 - Devolução de Livros')
-        print(' 6 - Relatorio da Biblioteca')
-        print('7 - Sair')
-
-        menu_opcoes = input('Digite a opção desejada: ')
+        menu_opcoes = input('\nDigite a opção desejada: ')
 
         if menu_opcoes == '1':
-             cadastro_users.cadastrar_usuario()
-
-        elif menu_opcoes == '2':    
-              cadastro_livros.cadastrar_livro()
-
-        elif menu_opcoes == '3':
+            cadastro_livros.cadastrar_livro()
+        
+        elif menu_opcoes == '2':
             editar_livro.editar_livros()
 
-        elif menu_opcoes == '4':
+        elif menu_opcoes == '3':
              emprestimo_livros.emprestimo_livros()
 
-        elif menu_opcoes == '5':
+        elif menu_opcoes == '4':
             devolucao_livros.devolver_livro()
 
-        elif menu_opcoes == '6':
+        elif menu_opcoes == '5':
             relatorio.relatorio_biblioteca()
 
-        elif menu_opcoes == '7':
+        elif menu_opcoes == '6':
             print('saindo...')
             break   
 
