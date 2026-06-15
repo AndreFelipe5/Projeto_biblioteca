f9d5259f8eb0539377662a6da84847f4de5d3919
AndreFelipe5 <afs69@discente.ifpe.edu.br>
2026-06-15 10:15:52 -0300

    corrgido erro no modulo emprestimo_livros

--- emprestimo_livros.py
+++ emprestimo_livros.py
@@ -1,30 +1,41 @@
 def emprestimo_livros():
     print("\n--------> Empréstimo de Livros <--------\n")
 
-   
     with open("livros.txt", "r") as arquivo:
         livros = [linha.strip() for linha in arquivo]
-    print("Lista de Livros disponíveis para Empréstimo:\n ")
+
+    if len(livros) == 0:
+        print("Não há livros disponiveis para emprestimo\n")
+        return
+
+    print("Lista de Livros disponíveis para Empréstimo:\n")
 
     for i, livro in enumerate(livros, start=1):
-        print((f'{i} - {livro}'))
+        print((f"{i} - {livro}"))
 
+    while True:
 
+        livro_escolhido = input("\nDigite o número do Livro para Empréstimo: ")
 
-        livro_escolhido = int(input('\nDigite o número do Livro para Empréstimo: '))
+        if livro_escolhido.isdigit():
+            livro_escolhido = int(livro_escolhido)
 
-        if 1 <= livro_escolhido <= len(livros):
-            livro_emprestado = livros.pop(livro_escolhido - 1)
+            if 1 <= livro_escolhido <= len(livros):
+                break
 
-            with open("livros.txt", "w") as arquivo:
-                for livro in livros:
-                    arquivo.write(livro + "\n")
-
-            with open("livros_emprestado.txt", "a") as arquivo:
-                arquivo.write(livro_emprestado + "\n")
-            print(f'\nO livro escolhido foi: {livro_emprestado}\n\nLivro emprestado com sucesso!\n')          
-            break
-                
+            else:
+                print("\nOpção invalida, Escolha um número dos livros listados.")
         else:
-            print('\nOpção inválida, Escolha um número dos Livros Listado\n')
-              
+            print("\nDigite apenas números.")
+
+    livro_emprestado = livros.pop(livro_escolhido - 1)
+
+    with open("livros.txt", "w") as arquivo:
+        for livro in livros:
+                arquivo.write(livro + "\n")
+
+    with open("livros_emprestado.txt", "a") as arquivo:
+        arquivo.write(livro_emprestado + "\n")
+
+    print(f"\nO livro escolhido foi: {livro_emprestado}")
+    print("Livro emprestado com sucesso!\n")
