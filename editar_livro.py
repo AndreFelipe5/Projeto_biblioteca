1592ee88d361a99fa0877a7919092a84202eff05
AndreFelipe5 <afs69@discente.ifpe.edu.br>
2026-06-18 13:54:15 -0300

    edição de livro e autor adicionados

--- editar_livro.py
+++ editar_livro.py
@@ -11,12 +11,14 @@ def editar_livros():
     print("\nLivros Cadastrados:\n")
 
     for i, livro in enumerate(livros, start=1):
-        print(f"{i} - {livro}")
+        titulo, autor = livro.split(";")
+        print(f'{i} - Titulo: {titulo}')
+        print(f' Autor: {autor}')
 
     while True:
 
         editar_livro = input("\nDigite o número do livro para Edita-lo: ")
-
+        
         if editar_livro.isdigit():
             editar_livro = int(editar_livro)
 
@@ -28,9 +30,33 @@ def editar_livros():
         else:
             print("Digite apenas números.")
 
-    novo_nome = input("\nDigite o Novo nome do Livro: ")
+    titulo_antigo, autor_antigo = livros[editar_livro - 1].split(";")
 
-    livros[editar_livro - 1] = novo_nome
+    print("\n1 - Editar Titulo")
+    print("\n2 - Editar Autor")
+    print("\n3 - Editar Titulo e Autor")
+
+    livro_escolhido = int(input(" Digite a Opção: "))
+
+    if livro_escolhido == 1:
+        novo_titulo = input("Novo Titulo: ").strip()
+        livros[editar_livro - 1] = f"{novo_titulo};{autor_antigo}"
+
+    elif livro_escolhido == 2:
+        novo_autor = input("Novo Autor: ").strip()
+        livros[editar_livro - 1] = f"{titulo_antigo};{novo_autor}"
+
+    elif livro_escolhido == 3:
+        novo_titulo = input("Novo Titulo: ").strip()
+        novo_autor = input("Novo Autor: ").strip()
+        livros[editar_livro - 1] = f"{novo_titulo};{novo_autor}"
+
+    elif livro_escolhido == 0:
+        return
+
+    else:
+        print('Opção Invalida')    
+        return
 
     with open("livros.txt", "w") as arquivo:
         for livro in livros:
